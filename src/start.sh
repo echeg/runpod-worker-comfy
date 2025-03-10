54a0b66cf6fdcf1d5f537be78be98a848489a916
Evgeniy Chegodaev <echegodaev@gmail.com>
2025-03-10 22:55:51 +0200

    0.1.22

--- src/start.sh
+++ src/start.sh
@@ -6,13 +6,13 @@ export LD_PRELOAD="${TCMALLOC}"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "runpod-worker-comfy: Starting ComfyUI"
+    echo "runpod-worker-comfy: Starting ComfyUI Locally"
     python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    echo "runpod-worker-comfy: Starting ComfyUI"
+    echo "runpod-worker-comfy: Starting ComfyUI in Server Mode"
     python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
